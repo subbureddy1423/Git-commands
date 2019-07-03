266447c2b7b527026e37b96c16875048f12305dd
sree <gsreddy1227@gmail.com>
2019-07-03 06:47:01 +0000

    commited by subba

---
+++ Ansibleinstallation.sh
@@ -0,0 +1,79 @@
+sh-keygen -t rsa
+  cd .ssh
+  ls
+  vi id_rsa.pub
+  cd /etc/
+  ls
+  cd /.ssh
+  cd .ssh
+  ssh
+  cd ssh
+  ls -l
+  cd sshd_config
+  vi sshd_config
+  ssh 13.235.75.226
+ yum install ansible
+  rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+  yum install ansible
+ ansible --version
+  cd
+  cd /etc/ansible
+  ls
+  vi hosts
+  cd
+  ansible -m ping client
+  ssh 13.235.75.226
+  ansible -m command -a"ls -la" client
+  ansible -m command -a"cd /opt" clinet
+  ansible -m command -a"cd /opt" client
+  ansible -m command -a"ls -la" client
+  ansible -m command -a"cd /opt" client
+  ansible -m command -a"cd /opt ls -la" client
+  ansible -m command -a"ls -la /opt" client
+  ansible -m command -a"ls -l/opt" client
+  ansible -m command -a"ls -l /opt" client
+  ansible -m command -a"yum install httpd" client
+  ansible -m command -a"yum install ansible" client
+  ansible -m command -a"yum install ansible -y" client
+  ansible -m service -a"name=ansible state=started" client
+  ansible -m command -a"name=ansible state=started" client
+  ansible -m command -a"systemctl start ansible" client
+  ansible -m command -a"yum install httpd -y" client
+ ansible -m comm" client
+  ansible -m service -a"name=httpd state=start" client
+  ansible -m service -a"name=httpd state=started" client
+  ansible -m command -a"systemctl stop httpd" client
+  cd /opt
+  touch file1
+  ansible -m file -a "src=/opt/file1 desc=/opt/file2" client
+  ansible -m file -a "src=file1 desc=/opt/file2" client
+  ansible -m file -a "src=file1 desc=/opt/file1" client
+  ansible -m file -a "src=file1 dest=/opt/file1" client
+  ansible -m file -a "src=file1 dest=/opt" client
+  ansible -m copy -a "src=file1 dest=/opt/file2" client
+  ansible -m file -a "path=/opt/file2 mode=777" client
+  rpm -qa| grep git
+ ls -ls
+  cd
+  vi .netrc
+  git init
+  ls
+  git status
+  git add anaconda-ks.cfg
+  git commit -m commit "commited by subbu"
+  git commit -m  "commited by subbu"
+ git push
+  git clone https://github.com/gsr1227/Start.git
+  cd Start
+  ls
+  touch file1
+  git add
+  git add .
+  git commit -m "commit by srikanth"
+  git push
+  cd ..
+  ls
+  ansible -m shell -a "sh abc.sh /opt" client
+  ansible -m shell -a "path=/opt sh abc.sh" client
+  ansible -m shell -a " sh abc.sh" client
+
